8077e230ea13bb7285e7282ffdcb857abd68286b
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2023-04-28 13:21:32 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -6,6 +6,85 @@
 
 #include "shell.h"
 
+int shelly_cd_helper(char **, char *, struct stat);
+int shelly_cd_helper2(char *, char *, char **);
+
+int shelly_cd_helper(char **args, char *oldpwd, struct stat dir)
+{
+	if (args[0])
+	{
+		if (*(args[0]) == '-' || shelly_strcmp(args[0], "--") == 0)
+		{
+			if ((args[0][1] == '-' && args[0][2] == '\0') ||
+				args[0][1] == '\0')
+			{
+				if (shelly_getenv("OLDPWD") != NULL)
+					(chdir(*shelly_getenv("OLDPWD") + 7));
+			}
+			else
+			{
+				free(oldpwd);
+				return (shelly_create_error(args, 2));
+			}
+		}
+		else
+		{
+			if (stat(args[0], &dir) == 0 && S_ISDIR(dir.st_mode) &&
+				((dir.st_mode & S_IXUSR) != 0))
+			{
+				chdir(args[0]);
+			}
+			else
+			{
+				free(oldpwd);
+				return (shelly_create_error(args, 2));
+			}
+		}
+	}
+	else
+	{
+		if (shelly_getenv("HOME") != NULL)
+			chdir(*(shelly_getenv("HOME")) + 5);
+	}
+}
+
+int shelly_cd_helper2(char *pwd, char *oldpwd, char **dir_info)
+{
+	pwd = getcwd(pwd, 0);
+	if (pwd == NULL)
+	{
+		free(oldpwd);
+		return (-1);
+	}
+
+	dir_info = malloc(sizeof(char *) * 2);
+	if (dir_info == NULL)
+	{
+		free(oldpwd);
+		free(pwd);
+		return (-1);
+	}
+
+	dir_info[0] = "OLDPWD";
+	dir_info[1] = oldpwd;
+	if (shelly_setenv(dir_info, dir_info) == -1)
+	{
+		free(oldpwd);
+		free(pwd);
+		free(dir_info);
+		return (-1);
+	}
+
+	dir_info[0] = "PWD";
+	dir_info[1] = pwd;
+	if (shelly_setenv(dir_info, dir_info) == -1)
+	{
+		free(oldpwd);
+		free(pwd);
+		free(dir_info);
+		return (-1);
+	}
+}
 /**
  * shelly_exit - Exits the shell.
  * @args: An array of arguments.
@@ -70,77 +149,8 @@ int shelly_cd(char **args, char __attribute__((__unused__)) **front)
 	if (oldpwd == NULL)
 		return (-1);
 
-	if (args[0])
-	{
-		if (*(args[0]) == '-' || shelly_strcmp(args[0], "--") == 0)
-		{
-			if ((args[0][1] == '-' && args[0][2] == '\0') ||
-				args[0][1] == '\0')
-			{
-				if (shelly_getenv("OLDPWD") != NULL)
-					(chdir(*shelly_getenv("OLDPWD") + 7));
-			}
-			else
-			{
-				free(oldpwd);
-				return (shelly_create_error(args, 2));
-			}
-		}
-		else
-		{
-			if (stat(args[0], &dir) == 0 && S_ISDIR(dir.st_mode) &&
-					((dir.st_mode & S_IXUSR) != 0))
-			{
-				chdir(args[0]);
-			}
-			else
-			{
-				free(oldpwd);
-				return (shelly_create_error(args, 2));
-			}
-		}
-	}
-	else
-	{
-		if (shelly_getenv("HOME") != NULL)
-			chdir(*(shelly_getenv("HOME")) + 5);
-	}
-
-	pwd = getcwd(pwd, 0);
-	if (pwd == NULL)
-	{
-		free(oldpwd);
-		return (-1);
-	}
-
-	dir_info = malloc(sizeof(char *) * 2);
-	if (dir_info == NULL)
-	{
-		free(oldpwd);
-		free(pwd);
-		return (-1);
-	}
-
-	dir_info[0] = "OLDPWD";
-	dir_info[1] = oldpwd;
-	if (shelly_setenv(dir_info, dir_info) == -1)
-	{
-		free(oldpwd);
-		free(pwd);
-		free(dir_info);
-		return (-1);
-	}
-
-	dir_info[0] = "PWD";
-	dir_info[1] = pwd;
-	if (shelly_setenv(dir_info, dir_info) == -1)
-	{
-		free(oldpwd);
-		free(pwd);
-		free(dir_info);
-		return (-1);
-	}
-
+	shelly_cd_helper(args, oldpwd, dir);
+	shelly_cd_helper2(pwd, oldpwd, dir_info);
 	if (args[0] && args[0][0] == '-' && args[0][1] != '-')
 	{
 		write(STDOUT_FILENO, pwd, shelly_strlen(pwd));
