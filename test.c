f11f1de463a7ca2cfd8c096a61805ef3994cbeaf
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2023-04-30 00:11:09 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,46 +1,37 @@
-ssize_t shelly_getline(char **lineptr, size_t *n, FILE *stream)
-{
-	static char buffer[120];
-	static ssize_t buf_size = 0;
-	ssize_t response = 0;
-	char x = 'x';
+static ssize_t input;
+	ssize_t response;
+	static char *buffer[SHELLY_READ_BUF_SIZE], x = 'x';
+	int r;
 
-	if (lineptr == NULL || n == NULL)
+	if (input == 0)
+		fflush(stream);
+	else
 		return (-1);
+	input = 0;
 
 	while (x != '\n')
 	{
-		if (buf_size == 0)
+		r = read(STDIN_FILENO, &x, 1);
+		if (r == -1 || (r == 0 && input == 0))
 		{
-			buf_size = read(STDIN_FILENO, buffer, sizeof(buffer));
-			if (buf_size == -1)
-				return (-1);
-			if (buf_size == 0 && input == 0)
-				return (-1);
+			free(buffer);
+			return (-1);
 		}
-
-		x = buffer[0];
-		buf_size--;
-		for (ssize_t i = 0; i < buf_size; i++)
+		if (r == 0 && input != 0)
 		{
-			buffer[i] = buffer[i + 1];
-		}
-
-		if (x == '\n')
-		{
-			if (shelly_appoint_lineptr(lineptr, n, buffer, response + 1) == NULL)
-				return (-1);
-			response++;
+			input++;
 			break;
 		}
-		else
-		{
-			if (shelly_realloc(lineptr, n, response, response + 1) == NULL)
-				return (-1);
-			(*lineptr)[response] = x;
-			response++;
-		}
-	}
 
+		if (input >= SHELLY_READ_BUF_SIZE)
+			buffer = shelly_realloc(buffer, input, input + 1);
+
+		buffer[input] = x;
+		input++;
+	}
+	buffer[input] = '\0';
+	shelly_appoint_lineptr(lineptr, n, buffer, input);
+	response = input;
+	if (r != 0)
+		input = 0;
 	return (response);
-}
