09b2ea8da1bea81760ff9e3a9abf3f897300a87b
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2023-05-16 13:43:36 +0100

    Update err_handler2.c

--- err_handler2.c
+++ err_handler2.c
@@ -4,25 +4,25 @@
  *          Princewill Chimdi Samuel
  */
 
-#include "shell.h"
+#include "shellx.h"
 
 /**
- * shelly_error_126 - Creates an error message for permission denied failures.
+ * shellx_error_126 - Creates an error message for permission denied failures.
  * @args: An array of arguments passed to the command.
  *
  * Return: The error string.
  */
-char *shelly_error_126(char **args)
+char *shellx_error_126(char **args)
 {
 	char *error, *history_str;
 	int length;
 
-	history_str = shelly_itoa(history);
+	history_str = shellx_itoa(history);
 	if (history_str == NULL)
 		return (NULL);
 
-	length = shelly_strlen(name) + shelly_strlen(history_str)
-		+ shelly_strlen(args[0]) + 24;
+	length = shellx_strlen(name) + shellx_strlen(history_str)
+		+ shellx_strlen(args[0]) + 24;
 	error = malloc(sizeof(char) * (length + 1));
 	if (!error)
 	{
@@ -30,34 +30,34 @@ char *shelly_error_126(char **args)
 		return (NULL);
 	}
 
-	shelly_strcpy(error, name);
-	shelly_strcat(error, ": ");
-	shelly_strcat(error, history_str);
-	shelly_strcat(error, ": ");
-	shelly_strcat(error, args[0]);
-	shelly_strcat(error, ": Permission denied\n");
+	shellx_strcpy(error, name);
+	shellx_strcat(error, ": ");
+	shellx_strcat(error, history_str);
+	shellx_strcat(error, ": ");
+	shellx_strcat(error, args[0]);
+	shellx_strcat(error, ": Permission denied\n");
 
 	free(history_str);
 	return (error);
 }
 
 /**
- * shelly_error_127 - Creates an error message for command not found failures.
+ * shellx_error_127 - Creates an error message for command not found failures.
  * @args: An array of arguments passed to the command.
  *
  * Return: The error string.
  */
-char *shelly_error_127(char **args)
+char *shellx_error_127(char **args)
 {
 	char *error, *history_str;
 	int length;
 
-	history_str = shelly_itoa(history);
+	history_str = shellx_itoa(history);
 	if (history_str == NULL)
 		return (NULL);
 
-	length = shelly_strlen(name) + shelly_strlen(history_str)
-		+ shelly_strlen(args[0]) + 16;
+	length = shellx_strlen(name) + shellx_strlen(history_str)
+		+ shellx_strlen(args[0]) + 16;
 	error = malloc(sizeof(char) * (length + 1));
 	if (!error)
 	{
@@ -65,12 +65,12 @@ char *shelly_error_127(char **args)
 		return (NULL);
 	}
 
-	shelly_strcpy(error, name);
-	shelly_strcat(error, ": ");
-	shelly_strcat(error, history_str);
-	shelly_strcat(error, ": ");
-	shelly_strcat(error, args[0]);
-	shelly_strcat(error, ": not found\n");
+	shellx_strcpy(error, name);
+	shellx_strcat(error, ": ");
+	shellx_strcat(error, history_str);
+	shellx_strcat(error, ": ");
+	shellx_strcat(error, args[0]);
+	shellx_strcat(error, ": not found\n");
 
 	free(history_str);
 	return (error);
