a5afaaab4203a1afd505ac3ea2947ea296e576d4
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2023-05-16 13:41:43 +0100

    Update err_handler.c

--- err_handler.c
+++ err_handler.c
@@ -4,26 +4,26 @@
  *          Princewill Chimdi Samuel
  */
 
-#include "shell.h"
+#include "shellx.h"
 
 /**
- * shelly_error_env - Creates an error message for shellby_env errors.
+ * shellx_error_env - Creates an error message for shellby_env errors.
  * @args: An array of arguments passed to the command.
  *
  * Return: The error string.
  */
-char *shelly_error_env(char **args)
+char *shellx_error_env(char **args)
 {
 	char *error, *history_str;
 	int len;
 
-	history_str = shelly_itoa(history);
+	history_str = shellx_itoa(history);
 	if (history_str == NULL)
 		return (NULL);
 
 	args--;
-	len = shelly_strlen(name) + shelly_strlen(history_str)
-		+ shelly_strlen(args[0]) + 45;
+	len = shellx_strlen(name) + shellx_strlen(history_str)
+		+ shellx_strlen(args[0]) + 45;
 	error = malloc(sizeof(char) * (len + 1));
 	if (error == NULL)
 	{
@@ -31,57 +31,57 @@ char *shelly_error_env(char **args)
 		return (NULL);
 	}
 
-	shelly_strcpy(error, name);
-	shelly_strcat(error, ": ");
-	shelly_strcat(error, history_str);
-	shelly_strcat(error, ": ");
-	shelly_strcat(error, args[0]);
-	shelly_strcat(error, ": Unable to add/remove from environment\n");
+	shellx_strcpy(error, name);
+	shellx_strcat(error, ": ");
+	shellx_strcat(error, history_str);
+	shellx_strcat(error, ": ");
+	shellx_strcat(error, args[0]);
+	shellx_strcat(error, ": Unable to add/remove from environment\n");
 
 	free(history_str);
 	return (error);
 }
 
 /**
- * shelly_error_1 - Creates an error message for shellby_alias errors.
+ * shellx_error_1 - Creates an error message for shellby_alias errors.
  * @args: An array of arguments passed to the command.
  *
  * Return: The error string.
  */
-char *shelly_error_1(char **args)
+char *shellx_error_1(char **args)
 {
 	char *error;
 	int length = 0;
 
-	length += shelly_strlen(name) + shelly_strlen(args[0]) + 13;
+	length += shellx_strlen(name) + shellx_strlen(args[0]) + 13;
 	error = malloc(sizeof(char) * (length + 1));
 	if (error == NULL)
 		return (NULL);
 
-	shelly_strcpy(error, "alias: ");
-	shelly_strcat(error, args[0]);
-	shelly_strcat(error, " not found\n");
+	shellx_strcpy(error, "alias: ");
+	shellx_strcat(error, args[0]);
+	shellx_strcat(error, " not found\n");
 
 	return (error);
 }
 
 /**
- * shelly_error_2_exit - Creates an error message for shellby_exit errors.
+ * shellx_error_2_exit - Creates an error message for shellby_exit errors.
  * @args: An array of arguments passed to the command.
  *
  * Return: The error string.
  */
-char *shelly_error_2_exit(char **args)
+char *shellx_error_2_exit(char **args)
 {
 	char *error, *history_str;
 	int len;
 
-	history_str = shelly_itoa(history);
+	history_str = shellx_itoa(history);
 	if (history_str == NULL)
 		return (NULL);
 
-	len = shelly_strlen(name) + shelly_strlen(history_str)
-		+ shelly_strlen(args[0]) + 27;
+	len = shellx_strlen(name) + shellx_strlen(history_str)
+		+ shellx_strlen(args[0]) + 27;
 	error = malloc(sizeof(char) * (len + 1));
 	if (!error)
 	{
@@ -89,34 +89,34 @@ char *shelly_error_2_exit(char **args)
 		return (NULL);
 	}
 
-	shelly_strcpy(error, name);
-	shelly_strcat(error, ": ");
-	shelly_strcat(error, history_str);
-	shelly_strcat(error, ": exit: Illegal number: ");
-	shelly_strcat(error, args[0]);
-	shelly_strcat(error, "\n");
+	shellx_strcpy(error, name);
+	shellx_strcat(error, ": ");
+	shellx_strcat(error, history_str);
+	shellx_strcat(error, ": exit: Illegal number: ");
+	shellx_strcat(error, args[0]);
+	shellx_strcat(error, "\n");
 
 	free(history_str);
 	return (error);
 }
 
 /**
- * shelly_error_2_cd - Creates an error message for cd errors.
+ * shellx_error_2_cd - Creates an error message for cd errors.
  * @args: An array of arguments passed to the command.
  *
  * Return: The error string.
  */
-char *shelly_error_2_cd(char **args)
+char *shellx_error_2_cd(char **args)
 {
 	char *error, *history_str;
 	int len;
 
-	history_str = shelly_itoa(history);
+	history_str = shellx_itoa(history);
 	if (history_str == NULL)
 		return (NULL);
 
-	len = shelly_strlen(name) + shelly_strlen(history_str)
-		+ shelly_strlen(args[0]) + 25;
+	len = shellx_strlen(name) + shellx_strlen(history_str)
+		+ shellx_strlen(args[0]) + 25;
 	if (args[0][0] == '-')
 		len += 15;
 	error = malloc(sizeof(char) * (len + 1));
@@ -126,37 +126,37 @@ char *shelly_error_2_cd(char **args)
 		return (NULL);
 	}
 
-	shelly_strcpy(error, name);
-	shelly_strcat(error, ": ");
-	shelly_strcat(error, history_str);
+	shellx_strcpy(error, name);
+	shellx_strcat(error, ": ");
+	shellx_strcat(error, history_str);
 	if (args[0][0] == '-')
-		shelly_strcat(error, ": cd: illegal option -- ");
+		shellx_strcat(error, ": cd: illegal option -- ");
 	else
-		shelly_strcat(error, ": cd: can't cd to ");
-	shelly_strcat(error, args[0]);
-	shelly_strcat(error, "\n");
+		shellx_strcat(error, ": cd: can't cd to ");
+	shellx_strcat(error, args[0]);
+	shellx_strcat(error, "\n");
 
 	free(history_str);
 	return (error);
 }
 
 /**
- * shelly_error_2_syntax - Creates an error message for syntax errors.
+ * shellx_error_2_syntax - Creates an error message for syntax errors.
  * @args: An array of arguments passed to the command.
  *
  * Return: The error string.
  */
-char *shelly_error_2_syntax(char **args)
+char *shellx_error_2_syntax(char **args)
 {
 	char *error, *history_str;
 	int length;
 
-	history_str = shelly_itoa(history);
+	history_str = shellx_itoa(history);
 	if (history_str == NULL)
 		return (NULL);
 
-	length = shelly_strlen(name) + shelly_strlen(history_str)
-		+ shelly_strlen(args[0]) + 33;
+	length = shellx_strlen(name) + shellx_strlen(history_str)
+		+ shellx_strlen(args[0]) + 33;
 	error = malloc(sizeof(char) * (length + 1));
 	if (!error)
 	{
@@ -164,14 +164,14 @@ char *shelly_error_2_syntax(char **args)
 		return (NULL);
 	}
 
-	shelly_strcpy(error, name);
-	shelly_strcat(error, ": ");
-	shelly_strcat(error, history_str);
-	shelly_strcat(error, ": ");
-	shelly_strcat(error, args[0]);
-	shelly_strcat(error, ": Syntax error: \"");
-	shelly_strcat(error, args[0]);
-	shelly_strcat(error, "\" unexpected\n");
+	shellx_strcpy(error, name);
+	shellx_strcat(error, ": ");
+	shellx_strcat(error, history_str);
+	shellx_strcat(error, ": ");
+	shellx_strcat(error, args[0]);
+	shellx_strcat(error, ": Syntax error: \"");
+	shellx_strcat(error, args[0]);
+	shellx_strcat(error, "\" unexpected\n");
 
 	free(history_str);
 	return (error);
